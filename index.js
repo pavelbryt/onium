da18abdcdc2b1cbc4269537769550c1425a64315
Pavel Bryt <pavel.bryt@gmail.com>
2017-06-05 21:21:32 +0300

    almost done?

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 'use strict';
 
 
-let Koa = require('koa');
-let KoaStaticCache = require('koa-static-cache');
+let HTTP = require('http');
 let Path = require('path');
 let SocketIO = require('socket.io');
+let URL = require('url');
+let send = require('send');
 
 let io = require('./lib/io');
 
 
-const PUBLIC_DIR = Path.join(__dirname, 'public');
-const VUE_DIR = Path.join(__dirname, 'node_modules', 'vue', 'dist');
-const SOCKET_IO_DIR = Path.join(__dirname, 'node_modules', 'socket.io-client', 'dist');
 const PORT = process.env.PORT || 9979;
 
+const FILES = Object.assign(Object.create(null), {
+	'/'                : Path.join(__dirname, 'public', 'index.html'),
+	'/index.html'      : Path.join(__dirname, 'public', 'index.html'),
+	'/index.css'       : Path.join(__dirname, 'public', 'index.css'),
+	'/index.js'        : Path.join(__dirname, 'public', 'index.js'),
+	'/vue.js'          : Path.join(__dirname, 'node_modules', 'vue', 'dist', 'vue.js'),
+	'/vue.min.js'      : Path.join(__dirname, 'node_modules', 'vue', 'dist', 'vue.min.js'),
+	'/socket.io.js'    : Path.join(__dirname, 'node_modules', 'socket.io-client', 'dist', 'socket.io.js'),
+	'/socket.io.js.map': Path.join(__dirname, 'node_modules', 'socket.io-client', 'dist', 'socket.io.js.map'),
+});
 
-let app = new Koa;
-let server = app.listen(PORT, () => console.log(PORT));
+
+let server = HTTP.createServer((req, res) => {
+	let { pathname } = URL.parse(req.url);
+	let file = FILES[pathname];
+	if (file) return send(req, file).pipe(res);
+	res.statusCode = 404;
+	res.end();
+}).listen(PORT, () => console.log(PORT));
 
 
 io.attach(server);
-
-app.use(KoaStaticCache(PUBLIC_DIR));
-app.use(KoaStaticCache(VUE_DIR));
-app.use(KoaStaticCache(SOCKET_IO_DIR));
--- public/index.js
+++ public/index.js
@@ -29,40 +29,28 @@ Vue.component('create-game', {
 
 	data() {
 		return {
-			movesPerPlayer: 10,
-			secondsPerMove: 10,
-			maxPlayers: 8,
+			roundsTotal: 10,
+			perMoveTime: 10000,
+			maxPlayers: 6,
 			isPrivate: false,
-			submitting: false,
 		};
+	},
+
+	computed: {
+		perMoveSeconds: {
+			get() {
+				return this.perMoveTime / 1e3;
+			},
+			set(seconds) {
+				this.perMoveTime = seconds * 1e3;
+			},
+		},
 	},
 
 	methods: {
 		submit() {
-			if (this.submitting) return;
-
-			let {
-				socket,
-				movesPerPlayer,
-				secondsPerMove,
-				maxPlayers,
-				isPrivate,
-				submitting
-			} = this;
-
-			this.submitting = true;
-
-			socket.emit('game:create', {
-				movesPerPlayer,
-				secondsPerMove,
-				maxPlayers,
-				isPrivate,
-			});
-
-			socket.once('game:created', (gameId) => {
-				this.$emit('gamecreate', gameId);
-				this.$emit('close');
-			});
+			this.socket.emit('gamecreate', this.$data);
+			this.$emit('close');
 		},
 	},
 });
@@ -72,166 +60,196 @@ Vue.component('create-game', {
 Vue.component('change-name', {
 	template: '#template-change-name',
 
-	props: ['prevName'],
+	props: ['name'],
 
 	data() {
-		return { name: this.prevName };
+		return {
+			input: this.name,
+		};
 	},
 
 	mounted() {
 		this.$refs.input.select();
-	}
-})
+	},
+});
 
 
 
 Vue.component('game', {
 	template: '#template-game',
 
-	props: ['id', 'playerName'],
+	props: ['id', 'username'],
 
 	data() {
 		return {
-			players: [],
-			player: null,
-			grid: null,
-			chat: [],
-			currentMessage: '',
-			isPrivate: null,
-			maxPlayers: null,
-			movesCount: null,
-			secondsPerMove: null,
-			started: null,
 			socket: null,
-			turn: 0,
+			thisPlayer: null,
+			turnStartTime: null,
+
+			roundsTotal: null,
+			perMoveTime: null,
+			maxPlayers: null,
+			isPrivate: null,
+			grid: null,
+			stage: null,
+			players: [],
+			round: null,
+			turn: null,
+			chat: [],
+			turnRemainingTime: null,
 		};
 	},
 
 	methods: {
-		initSocket(socket) {
-				// socket
-				// 	.on('error', console.error)
-				// 	.on('disconnect', () => console.log('disconnect'))
-				// 	.on('game:init', this.init)
-				// 	.on('game:playerjoin', (player) => this.players.push(player))
-				// 	.on('game:playerleave', (index) => this.players.splice(index, 1))
-				// 	.on('game:join', (index) => this.player = this.players[index])
-				// 	.on('game:start', this.onStart)
-				// 	.on('game:message', this.insertMessage)
-				// 	.on('invalid', console.warn);
-			socket
-				.on('error', console.error)
-				.on('gameerror', console.error)
+		initSocket() {
+			this.socket
 				.on('init', this.init)
-				.on('playerjoin', (player) => this.players.push(player))
-				.on('playerleave', (i) => this.players.splice(i, 1))
-				.on('join', (i) => this.player = this.players[i])
-				.on('message', this.insertMessage)
-				.on('shuffle', this.shuffle)
-				.on('start', () => this.started = true)
-				.on('turn', (i) => this.turn = i)
+				.on('playerjoin', this.addPlayer)
+				.on('playerleave', this.removePlayer)
+				.on('join', (i) => this.thisPlayer = this.players[i])
+				.on('chat', this.insertMessage)
+				.on('reposition', this.reposition)
+				.on('start', () => this.stage = 1)
+				.on('turn', this.nextTurn)
+				.on('round', (round) => this.round = round)
 				.on('move', this.rotate)
 				.on('clear', this.clear)
 				.on('fill', this.fill)
 				.on('points', (i, pts) => this.players[i].points = pts)
-				.on('end', this.end);
-		},
-
-		init(data) {
-			for (let key in data) this[key] = data[key];
+				.on('finish', this.finish)
+				.on('error', (error) => {
+					console.error(error);
+					this.$emit('socketerror', error);
+				});
 		},
 
 		join() {
-			this.socket.emit('join', this.playerName);
+			this.socket.emit('join', this.username);
 		},
 
-		insertMessage(name, message)  {
-			this.chat = this.chat.slice(-127).concat({ name, message });
+		leave() {
+			this.socket.emit('leave');
+		},
+
+		sendMessage(input) {
+			let message = { message: input };
+			if (!this.thisPlayer) message.name = this.username;
+			this.socket.emit('chat', message);
 		},
 
 		start() {
 			this.socket.emit('start');
 		},
 
-		shuffle(positions) {
+		move(r, q, reverse) {
+			if (!this.canMove) return;
+			if (this.neighborsOf({ r, q }).length != 6) return;
+			this.socket.emit('move', r, q, reverse);
+		},
+
+		init(data) {
+			this.thisPlayer = null;
+			for (let key in data) this[key] = data[key];
+
+			if (this.turnRemainingTime) {
+				this.turnStartTime = window.performance.now();
+			} else {
+				this.turnRemainingTime = this.perMoveTime;
+			}
+		},
+
+		addPlayer(player) {
+			this.players.push(player);
+		},
+
+		removePlayer(i) {
+			let { players } = this;
+			if (players[i] == this.thisPlayer) this.thisPlayer = null;
+			players.splice(i, 1);
+			if (i <= this.turn) this.turn--;
+		},
+
+		insertMessage(message)  {
+			this.chat = this.chat.slice(-127).concat(message);
+		},
+
+		reposition(positions) {
 			let { players } = this;
 			this.players = players.map((_, i) => players[positions[i]]);
 		},
 
-		sendMessage() {
-			if (!this.currentMessage) return;
-			this.socket.emit('game:message', this.currentMessage);
-			this.currentMessage = '';
+		nextTurn(i) {
+			this.turn = i;
+			this.turnStartTime = performance.now();
+			this.turnRemainingTime = this.perMoveTime;
 		},
 
-		hexAt([r, q]) {
-			let { grid } = this;
-			return grid[r] && grid[r][q];
-		},
+		rotate(r, q, reverse = false) {
+			this.turnStartTime = null;
 
-		setHex([r, q], hex) {
-			let { grid } = this;
-			this.$set(grid[r] || this.$set(grid, r, Array(q+1)), q, hex);
-			// (grid[r] || (grid[r] = Array(q+1)))[q] = hex;
-		},
+			let hex = this.hexAt(r, q);
+			if (!hex) return;
+			let neighbors = this.neighborsOf(hex);
+			if (reverse) neighbors.reverse();
 
-		prevNeighborsCoordsOf([r, q]) {
-			return [
-				[r  , q-1],
-				[r-1, q  ],
-				[r-1, q+1],
-			].filter(this.hexAt);
-		},
+			let { r: nr, q: nq } = neighbors[5];
 
-		nextNeighborsCoordsOf([r, q]) {
-			return [
-				[r  , q+1],
-				[r+1, q  ],
-				[r+1, q-1],
-			].filter(this.hexAt);
-		},
-
-		neighborsCoordsOf(coords) {
-			return this.prevNeighborsCoordsOf(coords)
-				.concat(this.nextNeighborsCoordsOf(coords));
-		},
-
-		rotate(coords, reverse = false) {
-			console.log(arguments);
-			let neighborsCoords = this.neighborsCoordsOf(coords);
-			if (reverse) neighborsCoords.reverse();
-			let neighbors = neighborsCoords.map(this.hexAt);
-
-			for (let i = 0; i < 6; i++) {
-				this.setHex(neighborsCoords[i], neighbors[i+1]);
+			for (let i = 5; i; i--) {
+				neighbors[i].r = neighbors[i-1].r;
+				neighbors[i].q = neighbors[i-1].q;
 			}
 
-			this.setHex(neighborsCoords[5], neighbors[0]);
+			neighbors[0].r = nr;
+			neighbors[0].q = nq;
 		},
 
-		clear(coordsArray) {
+		clear(indexes) {
 			let { grid } = this;
-			for (let [r, q] of coordsArray) this[r, q] = null;
+			for (let i of indexes) grid[i].color = -2;
 		},
 
-		emitMove(coords, reverse) {
-			if (!this.canMove) return;
-			if (this.neighborsCoordsOf(coords).length != 6) return;
-			this.socket.emit('move', coords, reverse);
+		fill(filled) {
+			let { grid } = this;
+			for (let [i, hex] of filled) this.$set(grid, i, hex);
+		},
+
+		finish() {
+			this.stage = 2;
+			this.turnRemainingTime = 0;
+		},
+
+		hexAt(r, q) {
+			let { grid } = this;
+			return grid && grid.find(hex => hex && hex.r == r && hex.q == q);
+		},
+
+		neighborsOf({ r, q }) {
+			return [
+				this.hexAt(r  , q-1),
+				this.hexAt(r-1, q  ),
+				this.hexAt(r-1, q+1),
+				this.hexAt(r  , q+1),
+				this.hexAt(r+1, q  ),
+				this.hexAt(r+1, q-1),
+			].filter(Boolean);
 		},
 	},
 
 	computed: {
+		canJoin() {
+			return !this.thisPlayer && this.stage == 0 && !this.full;
+		},
+
 		full() {
 			return this.players.length >= this.maxPlayers;
 		},
 
 		host() {
-			return !this.started && this.player == this.players[0];
+			return this.stage == 0 && this.thisPlayer == this.players[0];
 		},
 
 		canMove() {
-			return this.started && this.player == this.players[this.turn];
+			return this.stage == 1 && this.thisPlayer == this.players[this.turn];
 		},
 	},
 
@@ -246,10 +264,9 @@ Vue.component('game', {
 		socket: {
 			immediate: true,
 			handler(socket, oldSocket) {
-				console.log(socket, oldSocket);
 				if (oldSocket) oldSocket.close();
 				if (!socket) return;
-				this.initSocket(socket);
+				this.initSocket();
 			},
 		},
 	},
@@ -269,59 +286,41 @@ Vue.component('grid', {
 
 	data() {
 		return {
-			gridTransform: null,
-			viewBox: '0 0 20 20',
+			viewBox: null,
 		};
 	},
 
 	methods: {
-		async updateGridTransform() {
-			await this.$nextTick();
-			let bBox = this.$refs.svg.getBBox();
-			let scale = 1 / Math.max(bBox.width, bBox.height);
-			let translateX = -bBox.x * scale;
-			let translateY = -bBox.y * scale;
-			let matrix = [scale, 0, 0, scale, translateX, translateY];
-			this.gridTransform = `matrix(${matrix.join(',')})`;
+		updateViewBox() {
+			if (!this.grid) {
+				this.viewBox = null;
+				return;
+			}
+
+			let minX = +Infinity;
+			let minY = +Infinity;
+			let maxX = -Infinity;
+			let maxY = -Infinity;
+
+			for (let { r, q } of this.grid) {
+				let x = q + r / 2;
+				let y = r;
+				minX = Math.min(minX, x);
+				minY = Math.min(minY, y);
+				maxX = Math.max(maxX, x);
+				maxY = Math.max(maxY, y);
+			}
+
+			let x = minX * HEXAGON_WIDTH - HEXAGON_HALF_WIDTH;
+			let y = minY * HEXAGON_HEIGHT - HEXAGON_HALF_HEIGHT;
+			let w = (maxX - minX + 1) * HEXAGON_WIDTH;
+			let h = (maxY - minY + 1) * 3 * HEXAGON_QUARTER_HEIGHT + HEXAGON_QUARTER_HEIGHT;
+
+			this.viewBox = `${x} ${y} ${w} ${h}`;
 		},
 	},
 
 	computed: {
-		// viewBox() {
-		// 	let { hexes } = this.$refs;
-
-		// 	if (!hexes) return null;
-
-		// 	let maxX = 0, maxY = 0;
-
-		// 	for (let { x, y } of hexes) {
-		// 		maxX = Math.max(x, maxX);
-		// 		maxY = Math.max(y, maxY);
-		// 	}
-
-		// 	return `0 0 ${maxX} ${maxY}`;
-		// },
-
-		flatGrid() {
-			console.log('flatGrid');
-			let { grid } = this;
-			let flatGrid = [];
-
-			if (!grid) return grid;
-
-			for (let r = 0; r < grid.length; r++) {
-				let row = grid[r];
-				if (!row) continue;
-				for (let q = 0; q < row.length; q++) {
-					let hex = row[q];
-					if (!hex) continue;
-					flatGrid.push(Object.assign({ r, q }, hex));
-				}
-			}
-
-			return flatGrid.sort((a, b) => a.key - b.key);
-		},
-
 		hexagonPoints() {
 			return [
 				0, -HEXAGON_HALF_HEIGHT,
@@ -330,51 +329,20 @@ Vue.component('grid', {
 				0, HEXAGON_HALF_HEIGHT,
 				HEXAGON_HALF_WIDTH, HEXAGON_QUARTER_HEIGHT,
 				HEXAGON_HALF_WIDTH, -HEXAGON_QUARTER_HEIGHT,
+				// HEXAGON_HALF_WIDTH, 0,
+				// HEXAGON_WIDTH, HEXAGON_QUARTER_HEIGHT,
+				// HEXAGON_WIDTH, 3 * HEXAGON_QUARTER_HEIGHT,
+				// HEXAGON_HALF_WIDTH, HEXAGON_HEIGHT,
+				// 0, 3 * HEXAGON_QUARTER_HEIGHT,
+				// 0, HEXAGON_QUARTER_HEIGHT,
 			].join(',');
 		},
-
-		test() {
-			return [1, 2, 3];
-		}
 	},
 
 	watch: {
-		grid: {
-			// immediate: true,
-			handler(_, old) {
-				if (old) return;
-				console.log(this.$refs.hexes);
-				this.updateGridTransform();
-			},
+		grid(grid, oldGrid) {
+			if (grid != oldGrid) this.updateViewBox();
 		},
-	},
-
-	created() {
-		// this.socket.addEventListener('message', (event) => {
-		// 	try {
-		// 		let [...args] = JSON.parse(event.data);
-		// 		if (!args.length) return;
-		// 		this.$emit(...args);
-		// 	} catch (error) {}
-		// });
-
-		// let { grid } = this;
-		// let i = 0;
-
-		// for (let r in grid) {
-		// 	let row = grid[r];
-		// 	for (let q in row) {
-		// 		Object.assign(row[q], {
-		// 			grid,
-		// 			r: +r,
-		// 			q: +q,
-		// 			key: i++,
-		// 		});
-		// 	}
-		// }
-
-		// this.grid = grid;
-		// this.updateGridTransform();
 	},
 });
 
@@ -383,7 +351,17 @@ Vue.component('grid', {
 Vue.component('hex', {
 	template: '#template-hex',
 
-	props: ['r', 'q', 'id', 'type'],
+	props: ['hex'],
+
+	data() {
+		return {
+			scale: 1,
+			opacity: 1,
+			r: null,
+			q: null,
+			color: null,
+		};
+	},
 
 	computed: {
 		x() {
@@ -400,9 +378,155 @@ Vue.component('hex', {
 
 		style() {
 			return {
-				transform: `translate(${this.x}px,${this.y}px)`,
+				opacity: this.opacity,
+				transform: `translate(${this.x}px,${this.y}px) scale(${this.scale})`,
 			};
 		},
+	},
+
+	watch: {
+		hex: {
+			immediate: true,
+			deep: true,
+			handler(hex) {
+				if (hex == null) return;
+				this.r = hex.r;
+				this.q = hex.q;
+				this.color = hex.color;
+			}
+		}
+	}
+});
+
+
+
+Vue.component('lobby', {
+	template: '#template-lobby',
+
+	props: [
+		'stage',
+		'round',
+		'roundsTotal',
+		'turnRemainingTime',
+		'turnStartTime',
+		// 'latency',
+		'maxPlayers',
+		'players',
+		'thisPlayer',
+		'turn',
+		'chat',
+	],
+
+	data() {
+		return {
+			input: '',
+			remaining: null,
+			animationRequest: null,
+		};
+	},
+
+	methods: {
+		formatRemainingTime() {
+			let now = window.performance.now();
+			let passed = now - this.turnStartTime;
+			let remaining = Math.max(this.turnRemainingTime - passed, 0);
+
+			return (remaining / 1e3).toPrecision(3);
+		},
+
+		sendMessage() {
+			let { input } = this;
+			if (!input) return;
+			this.$emit('message', input);
+			this.input = '';
+		},
+	},
+
+	// mounted() {
+	// 	let animate = () => {
+	// 		// console.log('frame');
+	// 		this.animationRequest = window.requestAnimationFrame(animate);
+	// 		// this.remaining = this.formatRemainingTime();
+	// 		this.$forceUpdate();
+	// 	};
+
+	// 	animate();
+	// },
+
+	// beforeDestroy() {
+	// 	window.cancelAnimationFrame(this.animationRequest);
+	// },
+});
+
+
+
+Vue.component('timer', {
+	template: '#template-timer',
+
+	props: ['startTime', 'remainingTime'],
+
+	data() {
+		return {
+			frame: null,
+		};
+	},
+
+	computed: {
+		enabled() {
+			return Number.isFinite(this.remainingTime);
+		},
+
+		started() {
+			return this.enabled && Number.isFinite(this.startTime);
+		},
+	},
+
+	methods: {
+		formatRemainingTime() {
+			if (!this.enabled) {
+				this.stopAnimation();
+				return '--.--';
+			}
+
+			let remaining;
+
+			if (this.started) {
+				let now = window.performance.now();
+				let passed = now - this.startTime;
+				remaining = Math.max(this.remainingTime - passed, 0);
+				if (!remaining) this.stopAnimation();
+			} else {
+				remaining = this.remainingTime;
+				this.stopAnimation();
+			}
+
+			let fixed = (remaining / 1e3).toFixed(2);
+			return (fixed.length < 5 ? '0' : '') + fixed;
+		},
+
+		startAnimation() {
+			this.stopAnimation();
+			this.animate();
+		},
+
+		stopAnimation() {
+			window.cancelAnimationFrame(this.frame);
+		},
+
+		animate() {
+			this.frame = window.requestAnimationFrame(this.animate);
+			// console.log('frame');
+			this.$forceUpdate();
+		},
+	},
+
+	watch: {
+		startTime: 'startAnimation',
+		remainingTime: 'startAnimation',
+	},
+
+	beforeDestroy() {
+		this.stopAnimation();
 	},
 });
 
@@ -414,43 +538,42 @@ let app = new Vue({
 	data: {
 		socket: io(),
 		modal: null,
-		gameId: window.location.hash.slice(1),
-		playerName: localStorage.playerName || '',
+		gameId: window.location.hash.slice(1) || null,
+		username: window.localStorage.username || '',
 	},
 
 	methods: {
-		// enterGame(gameId) {
-		// 	this.leaveGame();
-		// 	this.modal = null;
-		// 	this.gameSocket = io(gameId);
-		// },
+		createGame() {
+			this.socket.emit('gamecreate');
+		},
 
-		// leaveGame() {
-		// 	let { gameSocket } = this;
-		// 	if (!gameSocket) return;
-		// 	gameSocket.close();
-		// 	this.gameSocket = null;
-		// },
+		quickGame() {
+			this.socket.emit('quickgame');
+		},
 	},
 
 	watch: {
 		gameId(id) {
-			window.location.hash = (id == null ? '' : id);
+			window.location.replace('#' + id);
 		},
 
-		playerName(name) {
-			localStorage.playerName = name;
+		username(name) {
+			window.localStorage.username = name;
 		},
 	},
 
 	created() {
-		this.socket.on('invalid', console.warn);
+		this.socket
+			.on('error', console.error)
+			.on('gameid', (gameId) => this.gameId = gameId);
 
-		if (!this.playerName) this.modal = 'change-name';
+		if (!this.username) this.modal = 'change-name';
 
 		window.addEventListener('keydown', ({ keyCode }) => {
 			if (keyCode == 27) this.modal = null;
 		});
+
+		if (!this.gameId) this.quickGame();
 	},
 });
 
